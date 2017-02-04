f598cc0fc6e4c1a63fafa42058a720de46ea4909
Jason Mohabir <jmohabir@stuy.edu>
2017-02-04 11:25:39 -0500

    Coded stuff

--- db_builder.py
+++ db_builder.py
@@ -1,9 +1,35 @@
 from pymongo import MongoClient
-import csv
 
+#facilitates CSV I/O
+import csv 
+
+
+#connect to server
+#server = MongoClient('localhost')
 server = MongoClient('127.0.0.1')
 
-foo1 = open("peeps.csv")
-foo2 = open("courses.csv")
-d1 = csv.DictReader(foo1)
-d2 = csv.DictReader(foo2)
+#open (or create) database
+db = server.mydb
+
+
+peeps_raw = open("peeps.csv")
+courses_raw = open("courses.csv")
+
+peeps = csv.DictReader(peeps_raw)
+courses = csv.DictReader(courses_raw)
+
+
+for student in peeps:
+    insertStudent = {} #the document
+    insertStudent['name'] = student['name']
+    insertStudent['age'] = student['age']
+    insertStudent['id'] = student['id']
+
+    grades = []
+    for course in courses:
+        if course['id'] == insertStudent['id']:
+            grades.append( { 'code' : course['code'], 'mark' : course['mark'] } )
+    insertStudent['courses'] = grades
+
+    #put doc into students collection
+    db.students.insert_one(oneStudent) 
